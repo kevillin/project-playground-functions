1b665968649ed98e06f4f008b64be0f22179e06c
Kevillin Santos <kevillin_santos@hotmail.com>
2022-05-18 15:00:43 -0300

    Requisito 2

--- src/challenges.js
+++ src/challenges.js
@@ -8,8 +8,9 @@ function compareTrue(param1, param2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let areaTringulo = (base * height) / 2;
+  return areaTringulo;
 }
 
 // Desafio 3
